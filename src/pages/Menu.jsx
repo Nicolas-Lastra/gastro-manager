cc25860ea32e16fae75f8b2e3a0d3654c7afa732
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-12-07 11:55:30 -0300

    MenuList component has been removed from the Menu page. Now, only one section is dinamically displayed based on the active section selected in the navbar.

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react"
-import MenuList from "../components/MenuList"
+import MenuSection from "../components/MenuSection"
+import MenuNavbar from "../components/MenuNavbar"
 
 export default function Menu() {
     const [loading, setLoading] = useState(true)
     const [businessName, setBusinessName] = useState('')
     const [menuSections, setMenuSections] = useState([])
+    const [currentSection, setCurrentSection] = useState([])
 
     useEffect(() => {
         async function fetchMenu() {
@@ -16,6 +18,7 @@ export default function Menu() {
 
                 setBusinessName(json.business.name)
                 setMenuSections(json.menu.sections)
+                setCurrentSection(json.menu.sections[0])
                 setLoading(false)
             } catch(error) {
                 console.error('Error fetching menu: ', error)
@@ -27,12 +30,18 @@ export default function Menu() {
         fetchMenu()
     }, [])
 
+    const handleSectionChange = (sectionId) => {
+        setCurrentSection(menuSections.find(item => item.id === sectionId))
+    }
+
     return (
         <main>
             <meta name="description" content="Edita tu carta como prefieras, establece precios, fotos y más." />
-            <h1>Menú</h1>
-            {loading && <h2>{businessName}</h2>}
-            <MenuList sections={menuSections} />
+            <h1>Menú - {businessName}</h1>
+            {loading && <small>Cargando ...</small>}
+            <MenuNavbar sections={menuSections} onSectionChange={handleSectionChange} currentSection={currentSection.id} />
+            {/* <MenuList sections={menuSections} /> */}
+            <MenuSection section={currentSection} />
         </main>
     )
 }
