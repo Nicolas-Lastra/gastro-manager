b0fcf2a8e424bee340e32655e09b8fc5238484e4
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-12-08 12:57:57 -0300

    GSAP animates the grid layout with a staggered animation.

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,6 +1,8 @@
 import MenuItemCard from "./MenuItemCard"
 import useSections from "../hooks/useSections"
 import MenuSubsection from "./MenuSubsection"
+import { useEffect, useRef } from "react"
+import gsap from "gsap"
 
 export function DisplayItems({ items }) {
 
@@ -16,9 +18,31 @@ export function DisplayItems({ items }) {
 export default function MenuSection({ section }) {
 
     const {hasSubsections, hasDirectItems} = useSections()
+    const sectionRef = useRef(null)
+
+    useEffect(() => {
+        if(!sectionRef.current) return
+        const cards = sectionRef.current.querySelectorAll("article")
+        if(!cards.length) return
+
+        // gsap.from(cards, {
+        //     opacity: 0,
+        //     y: 20,
+        //     duration: 0.4,
+        //     stagger: 0.06,
+        //     ease: 'power2.out'
+        // })
+
+        gsap.from(cards, {
+            scale: .9,
+            opacity: 0,
+            stagger: 0.06
+        })
+
+    }, [section])
     
     return (
-        <div className="menu-section">
+        <div className="menu-section" ref={sectionRef}>
             <h2>{section.name}</h2>
             {hasDirectItems(section) && <>
                 <div className="menu-subsection">
