eda2fc7ec37e83c7e82e68d201d7e69d4565bf95
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-12-13 20:51:33 -0300

    The global state of the tables is now obtained from the context using useTables.

--- src/pages/TableManagement.jsx
+++ src/pages/TableManagement.jsx
@@ -1,37 +1,14 @@
-import { useState } from "react"
-import { useEffect } from "react"
 import TableCard from "../components/TableCard"
+import { useTables } from "../context/TablesContext"
 
 export default function TableManagement() {
 
-    const [loading, setLoading] = useState(true)
-    const [tables, setTables] = useState([])
-
-    useEffect(() => {
-        async function fetchTables() {
-            try {
-                setLoading(true)
-                const response = await fetch('./tables.json')
-                const json = await response.json()
-
-                setTables(json.tables)
-                
-                setLoading(false)
-            } catch(error) {
-                console.error('Error fetching tables: ', error)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchTables()
-    }, [])
+    const { tables } = useTables()
 
     return(
         <main>
             <meta name="description" content="Edita tus mesas, cantidad de clientes, delivery y más." />
             <h1>Gestión de Mesas</h1>
-            {loading && <small>Cargando</small>}
             <div className="tables-container">
                 {tables.map((table) => (
                     <TableCard key={`table-${table.id}`} table={table}/>
