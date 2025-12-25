cbe041cfe8fbecfce86ac0a5a017293203b75353
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-12-25 13:46:29 -0300

    Routes wrapped in Suspense component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
 import Sidebar from './components/Sidebar.jsx'
 import { Route, Routes } from 'react-router'
-import { lazy } from 'react'
+import { lazy, useEffect, Suspense } from 'react'
 import { useBusinessStore } from './store/businessStore.jsx'
 import { useProductsStore } from './store/productsStore.jsx'
-import { useEffect } from 'react'
 
 const Home = lazy(() => import('./pages/Home.jsx'))
 const Menu = lazy(() => import('./pages/Menu.jsx'))
@@ -24,12 +23,15 @@ function App() {
   return (
     <>
       <Sidebar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/table-management"  element={<TableManagement /> } />
-        <Route path="/table-management/:tableId" element={<TableDetail />} />
-        <Route path="/menu" element={<Menu /> } />
-      </Routes>
+      <Suspense fallback={<div style={{ maxWidth: '1280px', margin: '0 auto', padding: '0 1rem' }}
+      >Cargando ...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/table-management"  element={<TableManagement /> } />
+          <Route path="/table-management/:tableId" element={<TableDetail />} />
+          <Route path="/menu" element={<Menu /> } />
+        </Routes>
+      </Suspense>
     </>
   )
 }
