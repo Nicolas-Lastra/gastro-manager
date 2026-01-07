5ff54dcf9f07d9eb0687cc5f04b5d009d7419a90
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2026-01-07 10:46:18 -0300

    Refactor SplitOrder component to include check number display and delete functionality

--- src/components/SplitOrder.jsx
+++ src/components/SplitOrder.jsx
@@ -1,24 +1,27 @@
 import { useTablesStore } from "../store/tablesStore"
-import { getAssignedQtyForLine, getAssignedQtyForLineInCheck } from "../utils/checks"
+import { getAssignedQtyForLine, getAssignedQtyForLineInCheck, getCheckNummber } from "../utils/checks"
 
-export default function SplitOrder({ table, selectedCheckId }) {
+export default function SplitOrder({ table, selectedCheckId, onDeleteSelectedCheck }) {
 
     const assignItemToCheck = useTablesStore((state) => state.assignItemToCheck)
     const unassignItemFromCheck = useTablesStore((state) => state.unassignItemFromCheck)
+    const checkNummber = getCheckNummber(table, selectedCheckId)
 
     return(
         <div>
-            <h2>Asignar</h2>
+            <div style={{display: 'flex', justifyContent: 'space-between', gap: '10px'}}>
+                <h2>Cuenta #{checkNummber ?? "-"}</h2>
+                <button onClick={() => selectedCheckId && onDeleteSelectedCheck(selectedCheckId)} style={{padding: '0rem .5rem'}}>X</button>
+            </div>
             <table>
                 <thead>
                     <tr>
                         <th>Producto</th>
-                        <th>Precio</th>
-                        <th>Total</th>
-                        <th>Asignado</th>
-                        <th>Restante</th>
+                        {/* <th>Precio</th> */}
+                        {/* <th>Total</th> */}
+                        {/* <th>Asignado</th> */}
+                        {/* <th>Restante</th> */}
                         <th>En cuenta</th>
-                        <th>Acción</th>
                     </tr>
                 </thead>
 
@@ -31,25 +34,28 @@ export default function SplitOrder({ table, selectedCheckId }) {
                     return (
                     <tr key={line.lineId}>
                         <td>{line.name}</td>
-                        <td>{line.price}</td>
-                        <td>{line.qty}</td>
-                        <td>{assigned}</td>
-                        <td>{remaining}</td>
-                        <td>{inThisCheck}</td>
+                        {/* <td>{line.price}</td> */}
+                        {/* <td>{line.qty}</td> */}
+                        {/* <td>{assigned}</td> */}
+                        {/* <td>{remaining}</td> */}
                         <td>
-                        <button
-                            disabled={remaining <= 0}
-                            onClick={() => assignItemToCheck(table.id, selectedCheckId, line.lineId, 1)}
-                        >
-                            +
-                        </button>
-
-                        <button
-                            disabled={inThisCheck <= 0}
-                            onClick={() => unassignItemFromCheck(table.id, selectedCheckId, line.lineId, 1)}
-                        >
-                            -
-                        </button>
+                            <div className="small-buttons-container">
+                                <button
+                                    className="small-button"
+                                    disabled={remaining <= 0}
+                                    onClick={() => assignItemToCheck(table.id, selectedCheckId, line.lineId, 1)}
+                                >
+                                    +
+                                </button>
+                                {inThisCheck}
+                                <button
+                                    className="small-button"
+                                    disabled={inThisCheck <= 0}
+                                    onClick={() => unassignItemFromCheck(table.id, selectedCheckId, line.lineId, 1)}
+                                >
+                                    -
+                                </button>
+                            </div>
                         </td>
                     </tr>
                     )
